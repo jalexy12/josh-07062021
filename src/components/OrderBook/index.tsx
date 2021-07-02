6d1387adc4f1159888800395e85482a762560566
Josh Alexy <jalexy12@gmail.com>
2021-07-02 10:58:13 -0500

    Adds colors for most elements

--- src/components/OrderBook/index.tsx
+++ src/components/OrderBook/index.tsx
@@ -20,43 +20,45 @@ export default function OrderBook() {
     <div className="order-book-container">
       <header>
         <h2>Order Book</h2>
+        <select>
+          <option>Group 0.50</option>
+          <option>Group 1</option>
+          <option>Group 2.5</option>
+        </select>
       </header>
-      <select>
-        <option>Group 0.50</option>
-        <option>Group 1</option>
-        <option>Group 2.5</option>
-      </select>
-      <div className="buy-container">
-        <ListWithHeader
-          headerTitle="Total"
-          listItems={randomArray(17, 2000, 200000)}
-        />
-        <ListWithHeader
-          headerTitle="Size"
-          listItems={randomArray(17, 2000, 50000)}
-        />
-        <ListWithHeader
-          headerTitle="Price"
-          listItems={randomArray(17, 47000, 47000)}
-        />
-      </div>
-      <div className="sell-container">
-        <ListWithHeader
-          headerTitle="Price"
-          listItems={randomArray(17, 47000, 47000)}
-        />
-        <ListWithHeader
-          headerTitle="Size"
-          listItems={randomArray(17, 2000, 50000)}
-        />
-        <ListWithHeader
-          headerTitle="Total"
-          listItems={randomArray(17, 2000, 200000)}
-        />
+      <div className="order-book-body">
+        <div className="buy-container">
+          <ListWithHeader
+            headerTitle="Total"
+            listItems={randomArray(17, 2000, 200000)}
+          />
+          <ListWithHeader
+            headerTitle="Size"
+            listItems={randomArray(17, 2000, 50000)}
+          />
+          <ListWithHeader
+            headerTitle="Price"
+            listItems={randomArray(17, 47000, 47000)}
+          />
+        </div>
+        <div className="sell-container">
+          <ListWithHeader
+            headerTitle="Price"
+            listItems={randomArray(17, 47000, 47000)}
+          />
+          <ListWithHeader
+            headerTitle="Size"
+            listItems={randomArray(17, 2000, 50000)}
+          />
+          <ListWithHeader
+            headerTitle="Total"
+            listItems={randomArray(17, 2000, 200000)}
+          />
+        </div>
       </div>
       <div className="order-book-footer">
-        <button>Toggle Feed</button>
-        <button>Kill Feed</button>
+        <button className="primary">Toggle Feed</button>
+        <button className="danger">Kill Feed</button>
       </div>
     </div>
   );
