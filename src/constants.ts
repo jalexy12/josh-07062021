5502eee9a480accc74bc6ad210babf2a335c8ce0
Josh Alexy <jalexy12@gmail.com>
2021-07-06 18:31:23 -0500

    Remove unneeded constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,4 @@
 const EVENT_TYPE = "subscribe";
 const FEED_TYPE = "book_ui_1";
-const BTC_PRODUCT = "PI_XBTUSD";
-const ETH_PRODUCT = "PI_ETHUSD";
 
-export { EVENT_TYPE, FEED_TYPE, BTC_PRODUCT, ETH_PRODUCT };
+export { EVENT_TYPE, FEED_TYPE };
